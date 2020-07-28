3e6379d5e163fca2e1267bb7fa0641d85c017b33
tranthimongle <tranlr15788@>
2020-07-28 16:09:44 +0700

    v1

--- lib/chatchat.dart
+++ lib/chatchat.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class ChatchatPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[Text('ndfsfg')],
+    );
+  }
+}
