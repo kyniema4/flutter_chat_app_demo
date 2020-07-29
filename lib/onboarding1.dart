523ff5a5d0d620d46018b2d9b23bfe475d9e0bf0
avoca <tran.chaugiangk39@gmail.com>
2020-07-29 08:40:33 +0700

    v1

--- lib/onboarding1.dart
+++ lib/onboarding1.dart
@@ -1,1 +1,48 @@
-
+import 'package:flutter/material.dart';
+import 'utils/theme.dart' as Theme;
+import 'package:carousel_slider/carousel_slider.dart';
+class OnBoardingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            color: Theme.Colors.purpleMain
+          ),
+          child: Center(
+            child: CarouselSlider(
+            options: CarouselOptions(
+              height: 400,
+              aspectRatio: 16/9,
+            
+              initialPage: 0,
+              enableInfiniteScroll: true,
+              reverse: false,
+              autoPlay: true,
+              autoPlayInterval: Duration(seconds: 3),
+              autoPlayAnimationDuration: Duration(milliseconds: 800),
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enlargeCenterPage: true,
+              // onPageChanged: callbackFunction,
+              scrollDirection: Axis.horizontal,
+            ),
+            items: [1,2,3,4,5].map((i) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.symmetric(horizontal: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.amber
+                    ),
+                    child: Text('text $i', style: TextStyle(fontSize: 16.0),)
+                  );
+                },
+              );
+            }).toList(),
+          ),
+          )
+        ),
+    );
+  }
+}
