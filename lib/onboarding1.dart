471992ec4b3bfd1d2a96fd886a5a7f2f7ba555c1
avoca <tran.chaugiangk39@gmail.com>
2020-08-05 10:46:07 +0700

    v1

--- lib/onboarding1.dart
+++ lib/onboarding1.dart
@@ -1,48 +1,144 @@
 import 'package:flutter/material.dart';
 import 'utils/theme.dart' as Theme;
 import 'package:carousel_slider/carousel_slider.dart';
-class OnBoardingPage extends StatelessWidget {
+
+final List<String> imgList = [
+  'assets/images/img_Boarding1.png', 
+  'assets/images/img-Boarding2.png',
+  'assets/images/img-Boarding3.png'
+];
+// var img;
+// var title;
+// var content;
+// List imgList = [
+//   {img: 'assets/images/img_Boarding1.png', title: 'Hello World', content: 'Welcome to learning Giang!'},
+//   {img: 'assets/images/img-Boarding2.png', title: 'Installation1', content: 'You can install React from npm.'},
+//   {img: 'assets/images/img-Boarding3.png', title: 'Installation2', content: 'You can install React from npm.'}
+// ];
+
+
+
+final List<Widget> imageSliders = imgList.map((item) {
+  return Builder(
+    builder: (BuildContext context) {
+      return Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.symmetric(horizontal: 40.0),
+        decoration: BoxDecoration(
+          color: Colors.amber
+        ),
+        child: Column(
+          children: <Widget>[
+          Image.asset(item, fit: BoxFit.contain, height: 230.0),
+            Column(
+              children: <Widget>[
+                Text(
+                  'giang',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  'this is giang',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ]
+            ),
+          ],
+        )
+      );
+    },
+  );
+}).toList();
+
+class OnBoardingPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _OnBoardingPageState();
+  }
+}
+class _OnBoardingPageState extends State<OnBoardingPage> {
+  int _current = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            color: Theme.Colors.purpleMain
-          ),
-          child: Center(
-            child: CarouselSlider(
-            options: CarouselOptions(
-              height: 400,
-              aspectRatio: 16/9,
-            
-              initialPage: 0,
-              enableInfiniteScroll: true,
-              reverse: false,
-              autoPlay: true,
-              autoPlayInterval: Duration(seconds: 3),
-              autoPlayAnimationDuration: Duration(milliseconds: 800),
-              autoPlayCurve: Curves.fastOutSlowIn,
-              enlargeCenterPage: true,
-              // onPageChanged: callbackFunction,
-              scrollDirection: Axis.horizontal,
-            ),
-            items: [1,2,3,4,5].map((i) {
-              return Builder(
-                builder: (BuildContext context) {
-                  return Container(
-                    width: MediaQuery.of(context).size.width,
-                    margin: EdgeInsets.symmetric(horizontal: 5.0),
-                    decoration: BoxDecoration(
-                      color: Colors.amber
-                    ),
-                    child: Text('text $i', style: TextStyle(fontSize: 16.0),)
-                  );
-                },
-              );
-            }).toList(),
-          ),
-          )
+      body: Container(
+        decoration: BoxDecoration(
+          color: Theme.Colors.purpleMain
         ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CarouselSlider(
+                  items: imageSliders,
+                  options: CarouselOptions(
+                    height: MediaQuery.of(context).size.height/1.8,
+                    viewportFraction: 1.0,
+                    enlargeCenterPage: false,
+                    enableInfiniteScroll: false,
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        _current = index;
+                      });
+                    }
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: imgList.map((url) {
+                    int index = imgList.indexOf(url);
+                    return Container(
+                      width: 8.0,
+                      height: 8.0,
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      alignment: Alignment.center,
+                      child: Icon(
+                        _current == index ? Icons.circle : Icons.lens_outlined,
+                          color: _current == index
+                          ? Color.fromRGBO(255, 255, 255, 1)
+                          : Color.fromRGBO(255, 255, 255, 0.3),
+                          size: 13.0
+                      )
+                    );
+                  }).toList(),
+                ),
+              ],  
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height/4,
+              padding: EdgeInsets.symmetric(horizontal: 40.0),
+              alignment: Alignment.center,
+              child: SizedBox(
+                width: double.infinity,
+                child:  RaisedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'LET\'S START',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontFamily: 'Proxima-Nova'
+                    ),),
+                  padding: EdgeInsets.symmetric(vertical: 18.0),
+                  // width: MediaQuery.of(context).size.width - 20,
+                ),
+              )
+            )
+          ]
+        ),
+      )
     );
   }
 }
