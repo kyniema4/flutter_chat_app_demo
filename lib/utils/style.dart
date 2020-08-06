06c638558aa2d3decc3a1239277b3e218a7d6ef7
avoca <tran.chaugiangk39@gmail.com>
2020-08-06 11:17:57 +0700

    v1

--- lib/utils/style.dart
+++ lib/utils/style.dart
@@ -4,11 +4,10 @@ import 'theme.dart' as Theme;
 final titleSlideStyle = TextStyle(
     color: Colors.white,
     fontSize: 30.0,
-    fontWeight: FontWeight.bold,
     fontFamily: 'Proxima-Nova');
 
 final contentSlideStyle =
-    TextStyle(color: Colors.white, fontSize: 17.0, fontFamily: 'Proxima-Nova');
+    TextStyle(color: Colors.white, fontSize: 17.0, fontFamily: 'Proxima-Nova-Regular');
 
 final letStartBtn = TextStyle(
     fontSize: 18.0,
